16180054a1f3d5be4575f8b964be7b8b8c23e795
zhangwentao <zhangwentao@wondersgroup.com>
2018-01-08 17:18:35 +0800

    添加修改数据事件

--- app/src/main/java/databind/wondersgroup/com/databindingdemo/CompoundActivity.java
+++ app/src/main/java/databind/wondersgroup/com/databindingdemo/CompoundActivity.java
@@ -1,15 +1,19 @@
 package databind.wondersgroup.com.databindingdemo;
 
+import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import databind.wondersgroup.com.databindingdemo.adapter.CompoundAdapter;
 import databind.wondersgroup.com.databindingdemo.databinding.ActivityCompoundBinding;
+import databind.wondersgroup.com.databindingdemo.listener.EventListener;
 import databind.wondersgroup.com.databindingdemo.model.CompoundModel;
 import databind.wondersgroup.com.databindingdemo.model.Contact;
 import databind.wondersgroup.com.databindingdemo.model.User;
@@ -27,6 +31,49 @@ public class CompoundActivity extends AppCompatActivity {
 
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
         initData();
+
+        //事件的实现
+        binding.setEvent(new EventListener() {
+            @Override
+            public void click1(View view) {
+                //修改recyclerview 上面的数据
+                compoundModels.clear();
+                for (int i = 0; i < 10; i++) {
+                    CompoundModel compoundModel = new CompoundModel();
+                    User user = new User();
+
+                    user.userAge.set("change  22=" + i);
+                    user.userName.set("change model=" + i + "   ");
+
+                    Contact contact1 = new Contact();
+                    contact1.setMail("change 81914343@qq.com");
+                    compoundModel.contacts.add(contact1);
+
+                    Contact contact = new Contact();
+                    contact.setMail("change ruby@sina.com");
+
+                    compoundModel.user.set(user);
+                    compoundModel.contacts.add(contact);
+
+                    compoundModels.add(compoundModel);
+
+                }
+
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void click2(View view) {
+
+            }
+
+            @Override
+            public void click3(View data) {
+
+            }
+
+        });
     }
 
     private void initData() {
